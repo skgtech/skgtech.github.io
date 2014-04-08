7fbb72843b65f486c05d6e2bc1880aa5a0b34ad7
Thanasis Polychronakis <thanpolas@gmail.com>
2014-04-09 01:07:04 +0300

    integrate browserify, switch flows

---
+++ _frontapp/app.js
@@ -0,0 +1,13 @@
+/*
+ * SKG Tech
+ * Thessaloniki Tech Events and Meetups
+ * https://github.com/skgtech/skgtech.github.io
+ *
+ * Copyright (c) 2014 SKG Tech Contributors.
+ * Licensed under the MIT license.
+ */
+/**
+ * @fileOverview Frontend application bootstrap file
+ */
+
+require('./frontpage');
