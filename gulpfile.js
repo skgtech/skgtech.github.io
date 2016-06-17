4a80cbb5a6ee03d4a3e8f8d13c43c64214cbe243
attheodo <at@atworks.gr>
2016-06-17 14:53:12 +0300

    watch for js folder changes

--- gulpfile.js
+++ gulpfile.js
@@ -56,7 +56,7 @@ gulp.task('sass', function () {
  */
 gulp.task('watch', function () {
     gulp.watch('_scss/**/*', ['sass']);
-    gulp.watch(['*.html', '_layouts/*.html', '_posts/*', '_includes/*.html', 'startups/*.html', 'about/*.html', 'projects/*.html', '_data/*.yaml'], ['jekyll-rebuild']);
+    gulp.watch(['*.html', '_layouts/*.html', '_posts/*', '_includes/*.html', 'startups/*.html', 'about/*.html', 'projects/*.html', '_data/*.yaml', 'js/*.js'], ['jekyll-rebuild']);
 });
 
 /**
